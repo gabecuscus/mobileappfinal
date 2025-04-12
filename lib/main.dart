83f8e52598ab54b8cf2b8c94498379747d7b2bc8
gabecuscus <gat84@miami.edu>
2025-04-12 14:15:59 -0400

    continue

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class CityWeather {
+  final String cityName;
+  final List<int> temperatureToday;
+  final List<String> startTimesToday;
+
+  CityWeather({
+    required this.cityName, 
+    required this.temperatureToday,
+    required this.startTimesToday
+    });
+}
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,21 +29,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,15 +40,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,27 +47,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Map<String, CityWeather> weatherData = {};
+  Future<void> fetchCityWeather(String cityKey, String gridUrl) async {
+    final response = await http.get(Uri.parse(gridUrl));
+    final Map<String, dynamic> data = jsonDecode(response.body);
+    final List periods = data['properties']['periods'];
 
+    final today = DateTime.now().toIso8601String().substring(0, 10);
+
+    final todayPeriods = periods
+        .where( (p)=> p['startTime'].startsWith(today) )
+        .take(15)
+        .toList();
+
+    final todayTemps = todayPeriods
+        .map<int>((p)=> p['temperature'] as int)
+        .toList();
+        // .where((p) => p['startTime'].startsWith(today))
+        // .take(10)
+        // .map<int>((p) => p['temperature'] as int)
+        // .toList();
+    
+    final startTimes = todayPeriods
+        .map<String>((p) {
+      final fullTime = DateTime.parse(p['startTime']);
+      final hour = fullTime.hour;
+      final formattedHour = hour % 12 == 0 ? 12 : hour % 12;
+      final period = hour >= 12 ? 'PM' : 'AM';
+      return '$formattedHour$period';
+    })
+    .toList();
+
+    
+
+    final cityWeather =
+        CityWeather(
+          cityName: cityKey, 
+          temperatureToday: todayTemps,
+          startTimesToday: startTimes
+          );
+
+    setState(() {
+      weatherData[cityKey] = cityWeather;
+    });
+  }
+
+
+  // List<Icon>     //rain icons
+  List<String> daysOfWeek = ['Mon', 'Tues', 'Wes', 'Thr', 'Fri', 'Sat', 'Sun'];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchCityWeather("miami",
+        'https://api.weather.gov/gridpoints/MFL/50,50/forecast/hourly');
+  }
+
+  int _counter = 0;
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // TRY THIS: Try changing the color here to a specific color (to
@@ -87,39 +123,125 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          // top main, curWeater ad city name
+          Column(
+            children: <Widget>[
+            Text('MIAMI'),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+                'Currentyl it is : ${weatherData['miami']!.temperatureToday[3]} F in Miami'),
+          ]),
+
+
+          Expanded(child: 
+          ListView(
+            children: <Widget>[
+              Text(
+                //////////////////////////////////////
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                ////////////////////////////////////
+                '$_counter',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+
+
+              Text(
+                  'The current time in Miami is ${DateTime.now().toIso8601String().substring(11, 16)}'),
+
+              // forecast of today
+              SizedBox(
+                height: 100,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    for(int i=0; i<weatherData['miami']!.temperatureToday.length; i++)
+                     Column(children: [
+                      Text(' ${weatherData['miami']!.temperatureToday[i]} '),
+                      Text('${weatherData['miami']!.startTimesToday[i]}'),
+                     ],),
+                    
+                  ],
+                ),
+              ),
+
+              // chance of rain
+              Row(
+                children: <Widget>[
+                  for(int i=0; i<7; i++)
+                    Text('  ${i}:00 PM')
+                  
+                  
+                ],
+              ),
+
+              // example horizontal scroll
+              SizedBox(
+                height: 100,
+                child: 
+                ListView(
+                // This next line does the trick.
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(width: 160, color: Colors.red),
+                  Container(width: 70, color: Colors.blue),
+                  Container(width: 160, color: Colors.green),
+                  Container(width: 160, color: Colors.yellow),
+                  Container(width: 160, color: Colors.orange),
+                ],
+              ),
+              ),
+
+              // temperature thrhought hte day
+              Row(
+                children: <Widget>[
+                  for(int i=0; i<7; i++)
+                    Text('  ${i}:00 AM')
+                  
+                  
+                ],
+
+              ),
+
+
+              //Temparature for the week
+              Row(
+                children: <Widget>[
+                  for(int i=0; i<7; i++)
+                    Text(daysOfWeek[i])
+                  
+                  
+                ],
+          
+              ),
+
+
+
+              
+              
+            ],
+          ),
+          )
+          
+
+
+        ],
+      )),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+
+      // bottomNavigationBar: NavigationBar(destinations: <Widget>[
+
+      //   NavigationDestination(icon: icon, label: label)
+      // ]),
     );
   }
 }
